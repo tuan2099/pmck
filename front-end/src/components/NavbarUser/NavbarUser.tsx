1100a2e34730da40126c7f39ac7180730a98a0c7
HoangAnhTuan <75159271+tuan2099@users.noreply.github.com>
2023-05-15 17:57:37 +0700

    up

--- front-end/src/components/NavbarUser/NavbarUser.tsx
+++ front-end/src/components/NavbarUser/NavbarUser.tsx
@@ -5,7 +5,7 @@ import logo from 'src/assets/logo.png'
 function NavbarUser() {
   return (
     <>
-      <div className='sticky left-0 right-0 top-0 flex items-center justify-between border-b border-[#e8ebed] bg-white px-7 py-2'>
+      <div className='sticky left-0 right-0 top-0 z-10 flex items-center justify-between border-b border-[#e8ebed] bg-white px-7 py-2'>
         <div className='w-[100px]'>
           <img className='w-full' src={logo} alt='logo website' />
         </div>
