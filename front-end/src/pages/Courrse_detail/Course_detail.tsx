7b8665738004d366781f4260693aa1ebdace345d
tuan2099 <gwowqkepspsld@gmail.com>
2023-05-31 22:33:45 +0700

    up

--- front-end/src/pages/Courrse_detail/Course_detail.tsx
+++ front-end/src/pages/Courrse_detail/Course_detail.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
 function Course_detail() {
+  const handleDuration = (duration: any) => {
+    console.log('Thời gian video:', duration)
+  }
   return (
     <>
       <div>
         <section className='m-auto w-full'>
-          <div className='relative z-[1] flex h-12 items-center bg-[#29303b]'>1</div>
+          <div className='relative z-[1] flex h-[50px] items-center bg-[#29303b]'></div>
           <div className='fixed bottom-[50px] right-0 top-0 z-[2] mt-[50px] w-[23%] border-l-2'>
             <div className='flex h-full w-full flex-col bg-white'>
               <header className='flex select-none items-center justify-between px-[16px] py-[12px]'>
@@ -40,12 +44,45 @@ function Course_detail() {
               </div>
             </div>
           </div>
-          <div className='bottonm-[50px] fixed left-0 top-0 mt-[500px] w-[77%] overflow-x-hidden overscroll-contain'>
-            <div className='relative w-full bg-black px-[8.5%]'></div>
+          <div className='bottonm-[50px] fixed left-0 top-0 mt-[50px] w-[77%] overflow-x-hidden overscroll-contain '>
+            <div className='w-full bg-black px-[8.5%]'>
+              <ReactPlayer
+                className=''
+                width='100%'
+                height='650px'
+                url='https://www.youtube.com/watch?v=5RvQt2KslpA'
+                controls={true}
+                onDuration={handleDuration}
+              />
+            </div>
           </div>
           <div className='fixed bottom-0 left-0 right-0 z-[2] flex h-[50px] items-center justify-center bg-[#f0f0f0]'>
-            <button className='uppercase'>bài trước</button>
-            <button className='ml-3 uppercase'>bài tiếp theo</button>
+            <button className='flex items-center uppercase'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='h-6 w-6'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+              </svg>
+              bài trước
+            </button>
+            <button className='ml-3 flex items-center rounded border-2 border-[#1e7115] px-[10px] py-[5px] uppercase text-[#1e7115]'>
+              bài tiếp theo{' '}
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='h-6 w-6'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+              </svg>
+            </button>
           </div>
         </section>
       </div>
