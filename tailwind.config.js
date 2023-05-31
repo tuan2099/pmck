fa7dd2dfb3b618278f08a2cdf314d031897380c0
TuyenPhamm <phamquangtuyen0704@gmail.com>
2023-05-31 10:17:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    extend: {
+      screens: {
+        '480': '480px',
+        '360': '360px',
+      },
+    },
+  },
+}
