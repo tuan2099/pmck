467ff1ca5f621290b8f0486b82bf34bf60181d68
HoangAnhTuan <75159271+tuan2099@users.noreply.github.com>
2023-06-01 15:51:56 +0700

    up

--- front-end/src/pages/Course_enrol/Course_enrol.tsx
+++ front-end/src/pages/Course_enrol/Course_enrol.tsx
@@ -188,7 +188,7 @@ function Course_enrol() {
                                   </svg>
                                   {lesson_item.attributes.title}
                                 </h5>
-                                <GetTimeYT className='text-[#979ba0]' link={lesson_item.attributes.video_url} />
+                                <GetTimeYT className='text-[#979ba0]' video_url={lesson_item.attributes.video_url} />
                               </div>
                             ))}
                           </div>
