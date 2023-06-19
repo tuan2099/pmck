75a0befcd498c4b90d1894b9d16ff3130dcad869
tuan2099 <gwowqkepspsld@gmail.com>
2023-06-19 22:35:22 +0700

    up

--- front-end/src/apis/learningprocess.api.ts
+++ front-end/src/apis/learningprocess.api.ts
@@ -3,6 +3,9 @@ import http from 'src/utils/https'
 const learningProcessApi = {
   getLearningProgresses() {
     return http.get('/learning-progresses')
+  },
+  getLesson() {
+    return http.get('/lesson-items?populate[learning_progresses][populate]=lesson_items')
   }
 }
 
