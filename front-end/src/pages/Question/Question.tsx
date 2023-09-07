06ed621824eb70c3f0123e43b4cfaf7fdc1d858f
tuan2099 <gwowqkepspsld@gmail.com>
2023-09-07 23:42:47 +0700

    up

--- front-end/src/pages/Question/Question.tsx
+++ front-end/src/pages/Question/Question.tsx
@@ -1,13 +1,13 @@
 import { InputAdornment, TextField } from '@mui/material'
-import { GridClearIcon, GridSearchIcon } from '@mui/x-data-grid'
-import { FaMailBulk, FaPhoneAlt, FaQuestionCircle, FaUsersCog } from 'react-icons/fa'
+import { GridSearchIcon } from '@mui/x-data-grid'
+import { FaLink, FaMailBulk, FaPhoneAlt, FaQuestion, FaQuestionCircle, FaUsersCog } from 'react-icons/fa'
 
 function Question() {
   return (
     <>
-      <div className='min-h-sceen mx-auto max-w-[1320px]  bg-white px-5'>
+      <div className='min-h-sceen mx-auto bg-white px-5'>
         <div className='mt-5 flex items-center justify-between'>
-          <h1 className='my-10 text-2xl font-semibold'>Frequently Asked Questions</h1>
+          <h1 className='my-10 text-2xl font-semibold'>Các câu hỏi thường gặp</h1>
           <div>
             <TextField
               size='small'
@@ -20,7 +20,6 @@ function Question() {
                 )
               }}
             />
-            <p className='text-[10px] italic'>* Lorem Ipsum is simply dummy text </p>
           </div>
         </div>
         <div>
@@ -35,7 +34,7 @@ function Question() {
                   <div className='flex justify-center text-[70px] text-color1'>
                     <FaUsersCog />
                   </div>
-                  <h3 className='text-center'>General</h3>
+                  <h3 className='text-center font-medium text-color1'>General</h3>
                 </div>
               ))}
             <div className='flex w-full cursor-pointer flex-col justify-between transition'>
@@ -57,7 +56,40 @@ function Question() {
               </div>
             </div>
           </div>
-          <div className='grid grid-cols-1 gap-4 lg:grid-cols-3'></div>
+          <div className='mt-[120px] grid grid-cols-1 gap-4 lg:grid-cols-3'>
+            <div className='w-full'>
+              <h3 className='flex items-center font-semibold text-color1'>
+                <FaQuestionCircle />
+                <span className='ml-2 text-xl'>Câu hỏi phổ biến</span>
+              </h3>
+              <ul className='mt-7 list-outside  text-color1'>
+                <li className='text-md list-inside list-disc font-medium'>
+                  Làm thế nào để tôi đăng ký và truy cập khóa học trực tuyến?
+                </li>
+                <li className='text-md font-medium'>Tôi cần có kiến thức kỹ thuật gì để tham gia vào khóa học?</li>
+                <li className='text-md font-medium'>
+                  Làm thế nào để tìm kiếm và chọn khóa học phù hợp với mục tiêu học tập của tôi?
+                </li>
+                <li className='text-md font-medium'>Cách làm bài tập và bài kiểm tra trực tuyến trong khóa học?</li>
+                <li className='text-md font-medium'>
+                  Làm thế nào để tải xuống tài liệu hoặc tài liệu tham khảo liên quan đến khóa học?
+                </li>
+              </ul>
+            </div>
+            <div className='w-full'>
+              <h3 className='flex items-center font-semibold text-color1'>
+                <FaLink />
+                <span className='ml-2 text-xl'>Liên kết hữu ích cho bạn.</span>
+              </h3>
+              <ul className='mt-7 list-outside pl-4 text-color1'>
+                <li className='text-md leading-10'>Lorem Ipsum is simply dummy text of the printing ?</li>
+                <li className='text-md leading-10'>Lorem Ipsum is simply dummy text of the printing ?</li>
+                <li className='text-md leading-10'>Lorem Ipsum is simply dummy text of the printing ?</li>
+                <li className='text-md leading-10'>Lorem Ipsum is simply dummy text of the printing ?</li>
+                <li className='text-md leading-10'>Lorem Ipsum is simply dummy text of the printing ?</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
