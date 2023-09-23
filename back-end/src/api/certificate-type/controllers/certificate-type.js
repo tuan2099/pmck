32a2ad0a6670a9a8169e88d987901b2b7f638b20
HoangAnhTuan <75159271+tuan2099@users.noreply.github.com>
2023-09-23 09:57:56 +0700

    up

---
+++ back-end/src/api/certificate-type/controllers/certificate-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * certificate-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::certificate-type.certificate-type');
---
+++ back-end/src/api/certificate-type/services/certificate-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * certificate-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::certificate-type.certificate-type');
